a048807c0ce334e106aa81bfce926b313a0043db
Abhishek Kumar <abhishekkumarjnvk@gmail.com>
2020-10-05 00:48:18 +0530

    Creting thumbnail

--- app.js
+++ app.js
@@ -3,9 +3,13 @@
 const fs = require('fs');
 const http = require('http');
 const PORT = 3330 || process.env.PORT;
-const src = "" //source of the pdf file for which thumbnail is to be created
+const src = "sample.pdf" //source of the pdf file for which thumbnail is to be created
 const hummus = require('hummus');
 const streams = require('memory-streams');
+const { input } = require('node-pdftocairo');
+const imageToBase64 = require('image-to-base64');
+
+
 const PDFRStreamForBuffer = require('./pdfr-stream-for-buffer.js');
 var Blob = require('blob');
 // https://stackoverflow.com/questions/42512982/node-js-get-the-first-page-of-pdf-buffer  => getting buffer array for the first page 
@@ -33,32 +37,44 @@ const getFirstPage = function (buffer) {
     return outStream.toBuffer();
 };
 function pdf2thumb(buf) {
-    let ResImg=buf;
+    let ResImg = buf;
 
     return ResImg;
-   
-  }
-  
+
+}
+
 
 http.createServer(function (req, res) {
-   fs.readFile(src,(err,data)=>{
-       try{
+    fs.readFile(src, async (err, data) => {
+
+        try {
             let firstPageBuffer = getFirstPage(data); //buffer array for the first page of the given document
-
-
-
-            var htmlElement = pdf2thumb(firstPageBuffer); //converting the buffer array of first page of pdf to html element to be returned
-            res.write( htmlElement,()=>{
-                console.log('first page displayed')
-            })
-           
-
-           
-
-            res.end()
+            var file_name = Math.random()
+            await input(firstPageBuffer, { format: 'png' }).output(file_name);      //saving a temporary file
+            imageToBase64(file_name + "-1.png")                                    //converting temporary file into base64 
+                .then(
+                    (response) => {
+                        fs.unlink(file_name+ "-1.png", (err => {                    //removing temporary file as our work is done
+                            if (err) console.log(err);
+                        }));
+                                        //rendering base64 string image
+                        var html_response = `<!DOCTYPE html>                          
+                        <html><body><img style='display:block; width:100px;height:100px;' id='base64image' src='data:image/jpeg;base64, ${response}' /></body></html>`
+                        res.write(html_response, () => {
+                            console.log('first page displayed')
+                        })
+                        
+                        res.end()
+                    }
+                )
+                .catch(
+                    (error) => {
+                        console.log(error); // Logs an error if there is
+                    }
+                )
         }
-        catch(err){
+        catch (err) {
             console.log(err);
         }
-   }) 
-  }).listen(PORT,()=>console.log(`Server Started @ ${PORT}`)); 
+    })
+}).listen(PORT, () => console.log(`Server Started @ ${PORT}`)); 
